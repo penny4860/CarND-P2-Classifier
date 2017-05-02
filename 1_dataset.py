6ee83f825731bcd505b0425e1edf0f40c8c3e3b0
jeongjoonsup <penny4860@gmail.com>
2017-05-02 23:16:25 +0900

    code 정리

--- 1_dataset.py
+++ 1_dataset.py
@@ -3,24 +3,12 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-import tensorflow as tf
-import tensorflow.contrib.slim as slim
-import numpy as np
-from scipy import io
-
-
-def load_dataset(filename):
-    data_dict = io.loadmat(filename)
-    images = data_dict["X"]
-    images = np.transpose(images, (3, 0, 1, 2))
-    labels = data_dict["y"]
-    return images, labels
+import utils
 
 
 if __name__ == '__main__':
     FILENAME = "dataset/train_32x32.mat"
-    
     # (N, 32, 32, 3), (N, 1)
-    images, labels = load_dataset(FILENAME)
+    images, labels = utils.load_dataset(FILENAME)
     print(images.shape, labels.shape)
 

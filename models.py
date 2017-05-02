c743b820e44a954495d2d55f4c4c44f78011a299
jeongjoonsup <penny4860@gmail.com>
2017-05-02 23:11:10 +0900

    models script added

---
+++ models.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+
+
+def cnn_model_batch_norm(images, is_training=True):
+    """
+    images : (N, 32, 32, 3)
+    """
+    batch_norm_params = {'is_training': is_training, 'decay': 0.9, 'updates_collections': None}
+    with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                        normalizer_fn=slim.batch_norm,
+                        normalizer_params=batch_norm_params):
+
+        # For slim.conv2d, default argument values are like
+        # normalizer_fn = None, normalizer_params = None, <== slim.arg_scope changes these arguments
+        # padding='SAME', activation_fn=nn.relu,
+        # weights_initializer = initializers.xavier_initializer(),
+        # biases_initializer = init_ops.zeros_initializer,
+        net = slim.conv2d(images, 32, [5, 5], scope='conv1')
+        net = slim.max_pool2d(net, [2, 2], scope='pool1')
+        net = slim.conv2d(net, 64, [5, 5], scope='conv2')
+        net = slim.max_pool2d(net, [2, 2], scope='pool2')
+        net = slim.flatten(net, scope='flatten3')
+
+        # For slim.fully_connected, default argument values are like
+        # activation_fn = nn.relu,
+        # normalizer_fn = None, normalizer_params = None, <== slim.arg_scope changes these arguments
+        # weights_initializer = initializers.xavier_initializer(),
+        # biases_initializer = init_ops.zeros_initializer,
+        net = slim.fully_connected(net, 1024, scope='fc3')
+        outputs = slim.fully_connected(net, 10, activation_fn=None, normalizer_fn=None, scope='fco')
+    return outputs

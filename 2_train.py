24d6c646178c491cc7f1eaa956fee032afc63995
jeongjoonsup <penny4860@gmail.com>
2017-05-03 14:33:08 +0900

    train code 수정

--- 2_train.py
+++ 2_train.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 
 import utils
 import tensorflow as tf
@@ -10,8 +10,8 @@ BATCH_SIZE = 120
 if __name__ == '__main__':
     FILENAME = "dataset/train_32x32.mat"
     # (N, 32, 32, 3), (N, 1)
-    images, labels, images_val, labels_val = utils.load_dataset(FILENAME)
-    print(labels.shape)
+    #images, labels, images_val, labels_val = utils.load_dataset(FILENAME)
+    data_train, data_val = utils.load_dataset(FILENAME)
 
     # Boolean for MODE of train or test
     is_training = tf.placeholder(tf.bool, name='MODE')
@@ -23,45 +23,42 @@ if __name__ == '__main__':
     # Predict
     y_pred = cnn_model_batch_norm(x)
  
-    loss = slim.losses.softmax_cross_entropy(y_pred, y)
+    cost_op = slim.losses.softmax_cross_entropy(y_pred, y)
  
     lr = 0.001
-    train_step = tf.train.AdamOptimizer(lr).minimize(loss)
+    train_op = tf.train.AdamOptimizer(lr).minimize(cost_op)
  
  
     # Get accuracy of model
     correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
  
  
     # Add ops to save and restore all the variables
     saver = tf.train.Saver()
-    sess = tf.InteractiveSession()
-    sess.run(tf.global_variables_initializer(), feed_dict={is_training: True})
- 
-    # Training cycle
-    total_batch = int(len(images) / BATCH_SIZE)
  
     # Loop for epoch
-    for epoch in range(10):
- 
-        # Random shuffling
- 
-        # Loop over all batches
-        for i in range(total_batch):
- 
-            # Compute the offset of the current minibatch in the data.
-            offset = (i * BATCH_SIZE) % len(images)
-            batch_xs = images[offset:(offset + BATCH_SIZE), :, :, :]
-            batch_ys = labels[offset:(offset + BATCH_SIZE), :]
- 
-            # Run optimization op (backprop), loss op (to get loss value)
-            # and summary nodes
-            _, train_accuracy = sess.run([train_step, accuracy] , feed_dict={x: batch_xs, y: batch_ys, is_training: True})
- 
-            # Display logs
-            print("Epoch:", '%04d,' % (epoch + 1),
-            "batch_index %4d/%4d, training accuracy %.5f" % (i, total_batch, train_accuracy))
+    print ("Train Start!!")
+    n_epoch = 5
+    # Todo : batch 로 나누는 구조
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        # sess.run(tf.global_variables_initializer(), feed_dict={is_training: True})
+
+        epoch = 0
+        while data_train.epochs_completed < n_epoch:
+            batch_xs, batch_ys = data_train.next_batch(BATCH_SIZE)
+
+            _, cost_value = sess.run([train_op, cost_op], feed_dict={x: batch_xs, y: batch_ys})
+            # print ("train cost: ", cost_value)
+
+            if epoch != data_train.epochs_completed:
+                epoch += 1
+                cost_value = sess.run(cost_op, feed_dict={x: data_val.images, y: data_val.labels})
+                accuracy = sess.run(accuracy_op, feed_dict={x: data_val.images, y: data_val.labels})
+                
+                print ("{}-epoch completed. validation cost : {}, {}".format(data_train.epochs_completed, cost_value, accuracy))
+        # saver.save(sess, save_file)
  
 #             # Save the current model if the maximum accuracy is updated
 #             if validation_accuracy > max_acc:

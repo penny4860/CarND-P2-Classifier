e8d10bd76bfadc154688f9fb598a6c8321ffb235
jeongjoonsup <penny4860@gmail.com>
2017-05-03 13:57:45 +0900

    return labels to one_hot

--- utils.py
+++ utils.py
@@ -11,5 +11,11 @@ def load_dataset(filename):
     data_dict = io.loadmat(filename)
     images = data_dict["X"]
     images = np.transpose(images, (3, 0, 1, 2))
-    labels = data_dict["y"]
-    return images, labels
+    labels = data_dict["y"].reshape(-1,)
+    labels[labels==10] = 0
+    
+    one_hot_encoding = np.zeros((len(labels), 10))
+    one_hot_encoding[np.arange(len(labels)), labels] = 1
+    one_hot_encoding = one_hot_encoding.reshape(-1, 10)
+
+    return images, one_hot_encoding

0481379d35f5eb9797f58c308a923196839aad0e
jeongjoonsup <penny4860@gmail.com>
2017-05-03 14:33:00 +0900

    driver interface 수정

--- 1_dataset.py
+++ 1_dataset.py
@@ -9,11 +9,11 @@ import utils
 if __name__ == '__main__':
     FILENAME = "dataset/train_32x32.mat"
     # (N, 32, 32, 3), (N, 1)
-    images, labels, images_val, labels_val = utils.load_dataset(FILENAME)
-    print(images.shape, labels.shape, images_val.shape, labels_val.shape)
+    train, val = utils.load_dataset(FILENAME)
+    print(train.images.shape, train.labels.shape, val.images.shape, val.labels.shape)
     
     import matplotlib.pyplot as plt
     
-    plt.imshow(images[0])
-    print(labels[0])
+    plt.imshow(train.images[0])
+    print(train.labels[0])
     plt.show()

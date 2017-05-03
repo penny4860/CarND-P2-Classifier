161da57d44771e483b81b40bf1f239b5e05bcb31
jeongjoonsup <penny4860@gmail.com>
2017-05-03 15:57:20 +0900

    comments

--- 2_train.py
+++ 2_train.py
@@ -10,25 +10,24 @@ if __name__ == '__main__':
     FILENAME = "dataset/train_32x32.mat"
     # (N, 32, 32, 3), (N, 1)
 
-    # 1. without BatchNorm
-    tf.reset_default_graph()
-    cls = net.ConvNet(BATCH_SIZE)
-    data_train, data_val = utils.load_dataset(FILENAME)
-    cls.train(data_train, data_val, "model1.ckpt", 2)
-
-    # 2. with BatchNorm
+    # 1. with BatchNorm : 90% in 5 epoches
     tf.reset_default_graph()
     cls = net.ConvNetBatchNorm(BATCH_SIZE)
     data_train, data_val = utils.load_dataset(FILENAME)
     cls.train(data_train, data_val, "model2.ckpt", 5)
 
- 
+    # 2. without BatchNorm : 75% in 5 epoches
+    tf.reset_default_graph()
+    cls = net.ConvNet(BATCH_SIZE)
+    data_train, data_val = utils.load_dataset(FILENAME)
+    cls.train(data_train, data_val, "model1.ckpt", 5)
+
 #             # Save the current model if the maximum accuracy is updated
 #             if validation_accuracy > max_acc:
 #                 max_acc = validation_accuracy
 #                 save_path = saver.save(sess, MODEL_DIRECTORY)
 #                 print("Model updated and saved in file: %s" % save_path)
  
-    print("Optimization Finished!")
+    print("Finished!")
 
     

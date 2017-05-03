fc90db9164013fa66c709737e2465b59c7dbf9ce
jeongjoonsup <penny4860@gmail.com>
2017-05-03 15:04:33 +0900

    is_training 을 명시

--- 2_train.py
+++ 2_train.py
@@ -42,20 +42,20 @@ if __name__ == '__main__':
     n_epoch = 5
     # Todo : batch 로 나누는 구조
     with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        # sess.run(tf.global_variables_initializer(), feed_dict={is_training: True})
+        # sess.run(tf.global_variables_initializer())
+        sess.run(tf.global_variables_initializer(), feed_dict={is_training: True})
 
         epoch = 0
         while data_train.epochs_completed < n_epoch:
             batch_xs, batch_ys = data_train.next_batch(BATCH_SIZE)
 
-            _, cost_value = sess.run([train_op, cost_op], feed_dict={x: batch_xs, y: batch_ys})
+            _, cost_value = sess.run([train_op, cost_op], feed_dict={x: batch_xs, y: batch_ys, is_training: True})
             # print ("train cost: ", cost_value)
 
             if epoch != data_train.epochs_completed:
                 epoch += 1
-                cost_value = sess.run(cost_op, feed_dict={x: data_val.images, y: data_val.labels})
-                accuracy = sess.run(accuracy_op, feed_dict={x: data_val.images, y: data_val.labels})
+                cost_value = sess.run(cost_op, feed_dict={x: data_val.images, y: data_val.labels, is_training: False})
+                accuracy = sess.run(accuracy_op, feed_dict={x: data_val.images, y: data_val.labels, is_training: False})
                 
                 print ("{}-epoch completed. validation cost : {}, {}".format(data_train.epochs_completed, cost_value, accuracy))
         # saver.save(sess, save_file)

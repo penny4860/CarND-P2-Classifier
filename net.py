28af4521536f0e0da1d6aaedf640ffd024f8bfbc
jeongjoonsup <penny4860@gmail.com>
2017-05-03 15:52:43 +0900

    batch norm 없는 conv model

--- net.py
+++ net.py
@@ -105,4 +105,37 @@ class ConvNetBatchNorm(Model):
         cost_op = tf.nn.softmax_cross_entropy_with_logits(logits=self.Y_pred, labels=self.Y)
         return cost_op
 
+class ConvNet(Model):
+    def build(self):
+        X = tf.placeholder(tf.float32, [None, 32, 32, 3])
+        Y = tf.placeholder(tf.float32, [None, 10])
+
+        with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                            normalizer_fn=None):
+    
+            # For slim.conv2d, default argument values are like
+            # normalizer_fn = None, normalizer_params = None, <== slim.arg_scope changes these arguments
+            # padding='SAME', activation_fn=nn.relu,
+            # weights_initializer = initializers.xavier_initializer(),
+            # biases_initializer = init_ops.zeros_initializer,
+            net = slim.conv2d(X, 32, [5, 5], scope='conv1')
+            net = slim.max_pool2d(net, [2, 2], scope='pool1')
+            net = slim.conv2d(net, 64, [5, 5], scope='conv2')
+            net = slim.max_pool2d(net, [2, 2], scope='pool2')
+            net = slim.flatten(net, scope='flatten3')
+    
+            # For slim.fully_connected, default argument values are like
+            # activation_fn = nn.relu,
+            # normalizer_fn = None, normalizer_params = None, <== slim.arg_scope changes these arguments
+            # weights_initializer = initializers.xavier_initializer(),
+            # biases_initializer = init_ops.zeros_initializer,
+            net = slim.fully_connected(net, 1024, scope='fc3')
+            Y_pred = slim.fully_connected(net, 10, activation_fn=None, normalizer_fn=None, scope='fco')
+
+        return X, Y, Y_pred
+
+    def cost(self):
+        # cost = tf.reduce_mean(tf.square(tf.subtract(self.Y_pred, self.Y)))
+        cost_op = tf.nn.softmax_cross_entropy_with_logits(logits=self.Y_pred, labels=self.Y)
+        return cost_op
 

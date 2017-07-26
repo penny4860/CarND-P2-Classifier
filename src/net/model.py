9d58f04cb09e74c4cc0200d222a2be84a5779485
jeongjoonsup <penny4860@gmail.com>
2017-07-26 23:08:33 +0900

    Revert "train / evaluate method extraction"
    This reverts commit 6163a7972485106841bf67d1c783176e8686c2da.

--- src/net/model.py
+++ src/net/model.py
@@ -4,57 +4,6 @@ import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
 
-def train(model, train_set, val_set, n_epoch, save_file=None, load_file=None):
-    saver = tf.train.Saver()
-    cost = model.cost()
-    optimizer = tf.train.AdamOptimizer(0.001, name="ADAM")
-    train_op = optimizer.minimize(cost)
-    
-    print ("Train Start!!")
-    # Todo : batch 로 나누는 구조
-    model.sess.run(tf.global_variables_initializer())
-    if load_file:
-        model.load_params(load_file)
-        accuracy = evaluate(model, val_set.images, val_set.labels)
-        print ("Network parameter loaded. validation accuracy : {}".format(accuracy))
-        max_acc = accuracy
-    else:
-        max_acc = 0
-
-    epoch = 0
-    while train_set.epochs_completed < n_epoch:
-        batch_xs, batch_ys = train_set.next_batch(model.batch_size)
-
-        _, cost_val = model.sess.run([train_op, cost], feed_dict={model.X: batch_xs, model.Y: batch_ys, model._is_training: True})
-        # print ("train cost: ", cost_val)
-
-        if epoch != train_set.epochs_completed:
-            epoch += 1
-            accuracy = evaluate(model, val_set.images, val_set.labels)
-            print ("{}-epoch completed. validation accuracy : {}".format(train_set.epochs_completed, accuracy))
-            
-            # Save the current model if the maximum accuracy is updated
-            if max_acc < accuracy and save_file:
-                max_acc = accuracy
-                saver.save(model.sess, save_file)
-                print("Model updated and saved in file: %s" % save_file)
-
-
-def evaluate(model, images, labels):    
-    """
-    # Args
-        images : (N, n_rows, n_cols, n_ch)
-        labels : (N, n_categories)
-    # Returns
-        accuracy : float
-    """
-    # Get accuracy of model
-    correct_prediction = tf.equal(tf.argmax(model.Y_pred, 1), tf.argmax(model.Y, 1))
-    accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    accuracy = model.sess.run(accuracy_op, feed_dict={model.X: images, model.Y: labels, model._is_training: False})
-    return accuracy
-
-
 class _Model:
     """
     # Attributes
@@ -84,6 +33,20 @@ class _Model:
     def cost(self):
         raise NotImplementedError
 
+    def evaluate(self, images, labels):
+        """
+        # Args
+            images : (N, n_rows, n_cols, n_ch)
+            labels : (N, n_categories)
+        # Returns
+            accuracy : float
+        """
+        # Get accuracy of model
+        correct_prediction = tf.equal(tf.argmax(self.Y_pred, 1), tf.argmax(self.Y, 1))
+        accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        accuracy = self.sess.run(accuracy_op, feed_dict={self.X: images, self.Y: labels, self._is_training: False})
+        return accuracy
+
     def load_params(self, load_file):
         """network parameter 를 load 하는 함수. """
         # 1. load save vars
@@ -100,3 +63,37 @@ class _Model:
         init_assign_op, init_feed_dict = slim.assign_from_checkpoint(load_file, saved_variables)
         self.sess.run(init_assign_op, init_feed_dict)
 
+    def train(self, train_set, val_set, n_epoch, save_file=None, load_file=None):
+        saver = tf.train.Saver()
+        cost = self.cost()
+        optimizer = tf.train.AdamOptimizer(0.001, name="ADAM")
+        train_op = optimizer.minimize(cost)
+        
+        print ("Train Start!!")
+        # Todo : batch 로 나누는 구조
+        self.sess.run(tf.global_variables_initializer())
+        if load_file:
+            self.load_params(load_file)
+            accuracy = self.evaluate(val_set.images, val_set.labels)
+            print ("Network parameter loaded. validation accuracy : {}".format(accuracy))
+            max_acc = accuracy
+        else:
+            max_acc = 0
+
+        epoch = 0
+        while train_set.epochs_completed < n_epoch:
+            batch_xs, batch_ys = train_set.next_batch(self.batch_size)
+
+            _, cost_val = self.sess.run([train_op, cost], feed_dict={self.X: batch_xs, self.Y: batch_ys, self._is_training: True})
+            # print ("train cost: ", cost_val)
+
+            if epoch != train_set.epochs_completed:
+                epoch += 1
+                accuracy = self.evaluate(val_set.images, val_set.labels)
+                print ("{}-epoch completed. validation accuracy : {}".format(train_set.epochs_completed, accuracy))
+                
+                # Save the current model if the maximum accuracy is updated
+                if max_acc < accuracy and save_file:
+                    max_acc = accuracy
+                    saver.save(self.sess, save_file)
+                    print("Model updated and saved in file: %s" % save_file)

0ae41a7cbc852acbbd379fd5053457bb80450ec1
jeongjoonsup <penny4860@gmail.com>
2017-07-26 19:50:38 +0900

    driver test 성공

--- 1_batch_norm.py
+++ 1_batch_norm.py
@@ -2,6 +2,7 @@
 
 import src.utils as utils
 from src.net.tiny_model import ConvNetBatchNorm, ConvNet
+from src.net.model import train
 import tensorflow as tf
 
 BATCH_SIZE = 120
@@ -14,13 +15,13 @@ if __name__ == '__main__':
     tf.reset_default_graph()
     cls = ConvNetBatchNorm(BATCH_SIZE)
     data_train, data_val = utils.load_dataset(FILENAME)
-    cls.train(data_train, data_val, 5)
+    train(cls, data_train, data_val, 5)
 
     # 2. without BatchNorm : 75% in 5 epoches
     tf.reset_default_graph()
     cls = ConvNet(BATCH_SIZE)
     data_train, data_val = utils.load_dataset(FILENAME)
-    cls.train(data_train, data_val, 5)
+    train(cls, data_train, data_val, 5)
 
     print("Finished!")
 

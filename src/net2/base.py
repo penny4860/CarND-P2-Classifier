a7836c74f22d83d0a5195bf2ae1105d5cdf5b147
jeongjoonsup <penny4860@gmail.com>
2017-07-27 20:41:35 +0900

    test driver 구현

--- src/net2/base.py
+++ src/net2/base.py
@@ -35,36 +35,6 @@ class _Model(object):
         return tf.reduce_mean(tf.cast(is_correct, tf.float32))
 
 
-class MnistCnn(_Model):
-
-    def _create_input_placeholder(self):
-        return tf.placeholder(tf.float32, [None, 28, 28, 1])
-
-    def _create_inference_op(self):
-        W1 = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev=0.01))
-        L1 = tf.nn.conv2d(self.X, W1, strides=[1, 1, 1, 1], padding='SAME')
-        L1 = tf.nn.relu(L1)
-        L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-        W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
-        L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
-        L2 = tf.nn.relu(L2)
-        L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-        W3 = tf.Variable(tf.random_normal([7 * 7 * 64, 256], stddev=0.01))
-        L3 = tf.reshape(L2, [-1, 7 * 7 * 64])
-        L3 = tf.matmul(L3, W3)
-        L3 = tf.nn.relu(L3)
-        
-        W4 = tf.Variable(tf.random_normal([256, 10], stddev=0.01))
-        model = tf.matmul(L3, W4)
-        return model
-
-    def _create_loss_op(self):
-        one_hot_y = tf.one_hot(self.Y, 10)
-        return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.inference_op, labels=one_hot_y))
-
-
 def train(model, X_train, y_train, batch_size=100, n_epoches=5):
     optimizer = tf.train.AdamOptimizer(0.001).minimize(model.loss_op)
     init = tf.global_variables_initializer()

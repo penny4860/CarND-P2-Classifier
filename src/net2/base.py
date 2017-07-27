ed4c22975b8f0a60a475956634477968f859d339
jeongjoonsup <penny4860@gmail.com>
2017-07-27 20:30:06 +0900

    net2.base module 구현

---
+++ src/net2/base.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+from abc import ABCMeta, abstractmethod
+
+class _Model(object):
+    
+    __metaclass__ = ABCMeta
+    
+    def __init__(self, input_tensor):
+        self.X = self._create_input_placeholder()
+        self.Y = self._create_output_placeholder()
+
+        self.inference_op = self._inference_op()
+        self.loss_op = self._loss_op()
+        self.accuracy_op = self._accuracy_op()
+
+    @abstractmethod
+    def _create_input_placeholder(self):
+        return tf.placeholder(tf.float32, [None, 28, 28, 1])
+
+    @abstractmethod
+    def _create_inference_op(self):
+        pass
+
+    @abstractmethod
+    def _create_loss_op(self):
+        one_hot_y = tf.one_hot(self.Y, 10)
+        return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.inference_op, labels=one_hot_y))
+
+    def _create_output_placeholder(self):
+        return tf.placeholder(tf.int64, [None])
+
+    def _create_accuracy_op(self):
+        is_correct = tf.equal(tf.argmax(self.inference_op, 1), self.Y)
+        return tf.reduce_mean(tf.cast(is_correct, tf.float32))
+
+
+class MnistCnn(_Model):
+
+    def _create_input_placeholder(self):
+        return tf.placeholder(tf.float32, [None, 28, 28, 1])
+
+    def _create_inference_op(self):
+        W1 = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev=0.01))
+        L1 = tf.nn.conv2d(self.X, W1, strides=[1, 1, 1, 1], padding='SAME')
+        L1 = tf.nn.relu(L1)
+        L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+        W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
+        L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
+        L2 = tf.nn.relu(L2)
+        L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+        W3 = tf.Variable(tf.random_normal([7 * 7 * 64, 256], stddev=0.01))
+        L3 = tf.reshape(L2, [-1, 7 * 7 * 64])
+        L3 = tf.matmul(L3, W3)
+        L3 = tf.nn.relu(L3)
+        
+        W4 = tf.Variable(tf.random_normal([256, 10], stddev=0.01))
+        model = tf.matmul(L3, W4)
+        return model
+
+    def _create_loss_op(self):
+        one_hot_y = tf.one_hot(self.Y, 10)
+        return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.inference_op, labels=one_hot_y))
+

5c483c156c8e85fc5b66b4a8d7792d310adeff07
jeongjoonsup <penny4860@gmail.com>
2017-07-29 11:40:31 +0900

    base update

--- src/main.py
+++ src/main.py
@@ -42,6 +42,7 @@ class _Model(object):
     def __init__(self):
         self.X = self._create_input_placeholder()
         self.Y = self._create_output_placeholder()
+        self.is_training = self._create_is_train_placeholder()
 
         self.inference_op = self._create_inference_op()
         self.loss_op = self._create_loss_op()
@@ -67,70 +68,100 @@ class _Model(object):
         is_correct = tf.equal(tf.argmax(self.inference_op, 1), self.Y)
         return tf.reduce_mean(tf.cast(is_correct, tf.float32))
 
+    def _create_is_train_placeholder(self):
+        is_training = tf.placeholder_with_default(tf.constant(False, dtype=tf.bool),
+                                                  shape=(),
+                                                  name='is_training')
+        return is_training
+
 
 def train(model, X_train, y_train, X_val, y_val, batch_size=100, n_epoches=5, ckpt=None):
-    optimizer = tf.train.AdamOptimizer(0.001).minimize(model.loss_op)
+
+    def _run_single_epoch(X_train, y_train, batch_size):
+        total_cost = 0
+        for offset, end in get_batch_index(len(X_train), batch_size):
+            _, cost_val = sess.run([optimizer, model.loss_op],
+                                   feed_dict={model.X: X_train[offset:end],
+                                              model.Y: y_train[offset:end],
+                                              model.is_training: True})
+            total_cost += cost_val
+        return total_cost
+   
+    def _save(sess, ckpt, global_step):
+        import os
+        directory = os.path.dirname(ckpt)
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+            
+        saver = tf.train.Saver()
+        saver.save(sess, ckpt, global_step=global_step)
+        # saver.save(sess, 'models/cnn')
+        # saver.save(sess, 'checkpoint_directory/model_name', global_step=model.global_step)
+
+    def _print_cost(epoch, cost, global_step):
+        print('Epoch: {:3d}, Training Step: {:5d}, Avg. cost ={:.3f}'.format(epoch + 1, global_step, cost))
+    
+    global_step = tf.Variable(0, dtype=tf.int32, trainable=False, name='global_step')
+    optimizer = tf.train.AdamOptimizer(0.001).minimize(model.loss_op, global_step=global_step)
     init = tf.global_variables_initializer()
 
     with tf.Session() as sess:
         sess.run(init)
-        total_batch = len(X_train) / batch_size
-        num_examples = len(X_train)
+        total_batch = get_n_batches(len(X_train), batch_size)
         
         for epoch in range(n_epoches):
-            total_cost = 0
-            
             X_train, y_train = shuffle(X_train, y_train)
-            for offset in range(0, num_examples, batch_size):
-                end = offset + batch_size
-                batch_x, batch_y = X_train[offset:end], y_train[offset:end]
-    
-                _, cost_val = sess.run([optimizer, model.loss_op],
-                                       feed_dict={model.X: batch_x,
-                                                  model.Y: batch_y})
-                total_cost += cost_val
-        
-            print('Epoch:', '%04d' % (epoch + 1),
-                  'Avg. cost =', '{:.3f}'.format(total_cost / total_batch))
+            cost = _run_single_epoch(X_train, y_train, batch_size)
+            _print_cost(epoch, cost / total_batch, sess.run(global_step))
+            
+            evaluate(model, X_train, y_train, sess, batch_size=batch_size)
+            evaluate(model, X_val, y_val, sess, batch_size=batch_size)
 
-            # evaluate(model, X_train, y_train, sess)
-            evaluate(model, X_val, y_val, sess)
+            if ckpt:
+                _save(sess, ckpt, global_step)
         
         print('Training done')
-        
-        if ckpt:
-            import os
-            directory = os.path.dirname(ckpt)
-            if not os.path.exists(directory):
-                os.mkdir(directory)
-                
-            saver = tf.train.Saver()
-            saver.save(sess, ckpt)
-            # saver.save(sess, 'models/cnn')
-            # saver.save(sess, 'checkpoint_directory/model_name', global_step=model.global_step)
 
-def evaluate(model, images, labels, session=None, ckpt=None):
+def evaluate(model, images, labels, session=None, ckpt=None, batch_size=100):
     """
     ckpt : str
         ckpt directory or ckpt file
     """
-    # Todo : accuracy op를 batch 별로 실행할 수 있도록 수정
-    # sample 숫자가 많으면 memory 문제로 평가가 불가능하다.
     def _evaluate(sess):
         if ckpt:
             saver = tf.train.Saver()
             saver.restore(sess, tf.train.latest_checkpoint(ckpt))
-            
-        print('Accuracy: ', sess.run(model.accuracy_op,
-                                        feed_dict={model.X: images,
-                                                   model.Y: labels}))
+
+        accuracy_value = 0
+        for offset, end in get_batch_index(len(images), batch_size):
+            accuracy_value += sess.run(model.accuracy_op,
+                                      feed_dict={model.X: images[offset:end],
+                                                 model.Y: labels[offset:end],
+                                                 model.is_training: False})
+        accuracy_value = accuracy_value / get_n_batches(len(images), batch_size)
+        return accuracy_value
 
     if session:
-        _evaluate(session)
+        accuracy = _evaluate(session)
     else:
         sess = tf.Session()
-        _evaluate(sess)
+        accuracy = _evaluate(sess)
         sess.close()
+        
+    print('Accuracy: {:.4f}'.format(accuracy))
+    return accuracy
+
+
+def get_batch_index(num_examples, batch_size=100):
+    for offset in range(0, num_examples, batch_size):
+        end = offset + batch_size
+        if end > num_examples:
+            break
+        yield (offset, end)
+
+def get_n_batches(num_examples, batch_size=100):
+    return int(num_examples / batch_size)
+
 ################################################################################################################
 
 class SignModel(_Model):

ccf0472a8e5011544d85b378ad7cb797110bfc66
jeongjoonsup <jeongjoonsup@melfas.com>
2017-08-02 15:55:46 +0900

    plot() 을 함수로 정리

--- src/step1.py
+++ src/step1.py
@@ -55,22 +55,35 @@ plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.rcdefaults()
-fig, ax = plt.subplots()
 
-# Example data
-labels = list(get_str_labels())
-# people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
-y_pos = np.arange(len(labels))
-#performance = 3 + 10 * np.random.rand(len(people))
-n_samples_per_label = get_n_samples_per_label()
+def plot_histogram(X, y, labels, title):
+    def _get_n_samples_for_classes(y, n_classes):
+        n_samples = []
+        for i in range(43):
+            n_samples.append(len(y[y==i]))
+        return n_samples
 
-ax.barh(y_pos, n_samples_per_label, align='center', color='green', ecolor='black')
-ax.set_yticks(y_pos)
-ax.set_yticklabels(labels)
-ax.invert_yaxis()  # labels read top-to-bottom
-ax.set_xlabel('number of samples for each label')
-ax.set_title('Training Sample Distribution')
- 
-plt.show()
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+
+    y_pos = np.arange(len(labels))
+    n_samples_for_classes = _get_n_samples_for_classes(y, len(labels))
+
+    ax.barh(y_pos, n_samples_for_classes, align='center', color='green', ecolor='black')
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(labels)
+    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_xlabel('number of samples for each label')
+    ax.set_title('{} Sample Distribution'.title)
+     
+    plt.show()
+
+
+dataframe = pd.read_csv("../signnames.csv")
+labels = list(dataframe['SignName'])
+plot_histogram(X_train, y_train, labels, "Training")
+plot_histogram(X_valid, y_valid, labels, "Valid")
+plot_histogram(X_test, y_test, labels, "Test")
+
+
 

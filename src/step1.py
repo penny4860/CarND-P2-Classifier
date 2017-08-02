50add4cdfab54f6cdc9ccc0fd182a2fcf815d2d9
jeongjoonsup <jeongjoonsup@melfas.com>
2017-08-02 16:00:27 +0900

    nit

--- src/step1.py
+++ src/step1.py
@@ -18,40 +18,7 @@ X_train, y_train = train['features'], train['labels']
 X_valid, y_valid = valid['features'], valid['labels']
 X_test, y_test = test['features'], test['labels']
 
-print(X_train.shape)
-
 import pandas as pd
-def int_to_str_label(i):
-    dataframe = pd.read_csv("../signnames.csv")
-    labels = dataframe['SignName']
-    return labels[i]
-
-def get_str_labels():
-    dataframe = pd.read_csv("../signnames.csv")
-    labels = dataframe['SignName']
-    return labels
-
-def get_n_samples_per_label():
-    n_samples = []
-    for i in range(43):
-        n_samples.append(len(y_train[y_train==i]))
-    return n_samples
-
-n_classes = 43
-for i in range(n_classes):
-    print("{:50s}: {}".format(int_to_str_label(i), len(y_train[y_train==i])))
-
-
-# http://matplotlib.org/examples/lines_bars_and_markers/barh_demo.html
-"""
-====================
-Horizontal bar chart
-====================
-
-This example showcases a simple horizontal bar chart.
-"""
-import matplotlib.pyplot as plt
-plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -59,7 +26,7 @@ import matplotlib.pyplot as plt
 def plot_histogram(X, y, labels, title):
     def _get_n_samples_for_classes(y, n_classes):
         n_samples = []
-        for i in range(43):
+        for i in range(n_classes):
             n_samples.append(len(y[y==i]))
         return n_samples
 
